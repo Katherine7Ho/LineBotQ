374b7d671e5dab4df92776be848f8fc792075fb6
Katherine7Ho <katherineho0706@gmail.com>
2022-01-12 18:35:58 +0800

    change app.py

--- app.py
+++ app.py
@@ -275,7 +275,7 @@ def pushMessage(payload):
 
 
 def getTotalSentMessageCount():
-    response =requests.post('https://api.line.me/v2/bot/message/quota/consumption', headers=HEADER, data=json.dumps())
+    response =requests.get('https://api.line.me/v2/bot/message/quota/consumption', headers=HEADER)
     print(response.text)
     return response.json()['totalUsage']
 
