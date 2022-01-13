bfe07ac3c40b60476e45bc459422bdf063e2fa1f
Katherine7Ho <katherineho0706@gmail.com>
2022-01-13 15:45:58 +0800

    change app.py

--- app.py
+++ app.py
@@ -258,13 +258,13 @@ def getMRTSoundMessage():
     message["type"] = "audio"
     message["originalContentUrl"] = F"{end_point}/static/mrt_sound.m4a"
     import audioread
-    with audioread.audio_open("/static/mrt_sound.m4a") as f:
-        # totalsec contains the length in float
-        totalsec = f.duration
-    message["duration"] = totalsec * 1000
+    # with audioread.audio_open("/static/mrt_sound.m4a") as f:
+    #     # totalsec contains the length in float
+    #     totalsec = f.duration
+    # message["duration"] = totalsec * 1000
     # message["duration"] = 1000
-    print(os.getcwd())
-    return message
+    # print(os.getcwd())
+    return os.getcwd()
 
 
 def getTaipei101ImageMessage(originalContentUrl=F"{end_point}/static/taipei_101.jpeg"):
