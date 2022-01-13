9ab56730e4cabb848c120424c66711ef83d92530
Katherine7Ho <katherineho0706@gmail.com>
2022-01-13 16:15:56 +0800

    change app.py

--- app.py
+++ app.py
@@ -96,6 +96,100 @@ def index():
                               }
                             }
                         ]
+                elif text == "Confirm template":
+                    payload["messages"] = [
+                            {
+                                "type": "template",
+                                "altText": "this is a confirm template",
+                                "template": {
+                                    "type": "confirm",
+                                    "text": "Are you sure?",
+                                    "actions": [
+                                        {
+                                            "type": "message",
+                                            "label": "Yes",
+                                            "text": "yes"
+                                        },
+                                        {
+                                            "type": "message",
+                                            "label": "No",
+                                            "text": "no"
+                                        }
+                                    ]
+                                }
+                                }
+                    ]
+                elif text == "Carousel template":
+                    payload["messages"] = [
+                            {
+                                "type": "template",
+                                "altText": "this is a carousel template",
+                                "template": {
+                                    "type": "carousel",
+                                    "columns": [
+                                        {
+                                            "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
+                                            "imageBackgroundColor": "#FFFFFF",
+                                            "title": "this is menu",
+                                            "text": "description",
+                                            "defaultAction": {
+                                                "type": "uri",
+                                                "label": "View detail",
+                                                "uri": "http://example.com/page/123"
+                                            },
+                                            "actions": [
+                                                {
+                                                    "type": "postback",
+                                                    "label": "Buy",
+                                                    "data": "action=buy&itemid=111"
+                                                },
+                                                {
+                                                    "type": "postback",
+                                                    "label": "Add to cart",
+                                                    "data": "action=add&itemid=111"
+                                                },
+                                                {
+                                                    "type": "uri",
+                                                    "label": "View detail",
+                                                    "uri": "http://example.com/page/111"
+                                                }
+                                            ]
+                                        },
+                                        {
+                                            "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+                                            "imageBackgroundColor": "#000000",
+                                            "title": "this is menu",
+                                            "text": "description",
+                                            "defaultAction": {
+                                                "type": "uri",
+                                                "label": "View detail",
+                                                "uri": "http://example.com/page/222"
+                                            },
+                                            "actions": [
+                                                {
+                                                    "type": "postback",
+                                                    "label": "Buy",
+                                                    "data": "action=buy&itemid=222"
+                                                },
+                                                {
+                                                    "type": "postback",
+                                                    "label": "Add to cart",
+                                                    "data": "action=add&itemid=222"
+                                                },
+                                                {
+                                                    "type": "uri",
+                                                    "label": "View detail",
+                                                    "uri": "http://example.com/page/222"
+                                                }
+                                            ]
+                                        }
+                                    ],
+                                    "imageAspectRatio": "rectangle",
+                                    "imageSize": "cover"
+                                }
+                                }
+                    ]
+
                 else:
                     payload["messages"] = [
                             {
@@ -258,7 +352,7 @@ def getMRTSoundMessage():
     message["type"] = "audio"
     message["originalContentUrl"] = F"{end_point}/static/mrt_sound.m4a"
     import audioread
-    with audioread.audio_open("/mrt_sound.m4a") as f:
+    with audioread.audio_open("/app/mrt_sound.m4a") as f:
         # totalsec contains the length in float
         totalsec = f.duration
     message["duration"] = totalsec * 1000
